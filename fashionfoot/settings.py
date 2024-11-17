bb480ed5ca49ba05d952be15c1af863bf605a3d1
shiva <s3r3shiva@gmail.com>
2024-11-17 16:42:56 +0530

    updated settings.py 3

--- fashionfoot/settings.py
+++ fashionfoot/settings.py
@@ -37,12 +37,6 @@ CSRF_TRUSTED_ORIGINS = ['https://fashionfoot-production.up.railway.app']
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Database-backed sessions
 
 
-SESSION_COOKIE_DOMAIN = 'fashionfoot-production.up.railway.app'
-CSRF_COOKIE_DOMAIN = 'fashionfoot-production.up.railway.app'
-
-
-SESSION_COOKIE_SECURE = True  # Use True if using HTTPS
-CSRF_COOKIE_SECURE = True     # Use True if using HTTPS
 
 
 # Application definition
