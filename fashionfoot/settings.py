b26b6a895f10d3cc6324d970af8d962b6712d77a
shiva <s3r3shiva@gmail.com>
2024-11-17 13:35:46 +0530

    updated settings.py

--- fashionfoot/settings.py
+++ fashionfoot/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'fashionfoot.urls'
